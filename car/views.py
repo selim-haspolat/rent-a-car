61926df6282148f408dfa855860844039f6f48bf
selim-haspolat <118964736+selim-haspolat@users.noreply.github.com>
2023-06-13 22:43:33 +0300

    start and end date

--- car/views.py
+++ car/views.py
@@ -28,9 +28,29 @@ class CarView(FixView):
             queryset = Car.objects.all() # Eğer user.is_staff ise tüm veriyi göster.
         else:
             queryset = super().get_queryset() # Default veriyi göster.
-        
-        return queryset
 
+        # Tarihe göre filtrele:
+        # https://localhost/api/car/?from=2023-01-20&to=2023-01-25
+        start = self.request.query_params.get('from', None)
+        end = self.request.query_params.get('to', None)
+
+        if start and end:
+
+            # not_available_car_ids = Reservation.objects.filter(
+            #     start_date__gte = start, start_date__lte = end
+            # ).values_list('car_id', flat=True)
+
+            # AND ve OR kullanmak için Q parametresini kullabiliriz:
+            from django.db.models import  Q
+            not_available_car_ids = Reservation.objects.filter(
+                (Q(start_date__gte = start) and Q(start_date__lte = end))
+                or
+                (Q(end_date__gte = start) and Q(end_date__lte = end))
+            ).values_list('car_id', flat=True)
+
+            queryset = queryset.exclude(id__in=not_available_car_ids)
+
+        return queryset
 
 # -----------------------------------------
 # ReservationView
